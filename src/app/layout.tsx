a4684e017359c5c475a98b8aea7cffe959b8d2e1
durksteedjr <durksteedjr@proton.me>
2023-07-22 12:32:44 -0400

    chore(next): create next app

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+import type { LayoutProps } from "../types";
+
+const Layout = ({ children }: LayoutProps) => (
+  <html lang="en">
+    <body>{children}</body>
+  </html>
+);
+
+export default Layout;
