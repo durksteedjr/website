b46690c9a6ac5e242210b7e0c225e4e997a8b07e
durksteedjr <durksteedjr@proton.me>
2023-07-22 12:42:02 -0400

    chore(tailwindcss): add tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 
-const Page: NextPage = () => "website";
+const Page: NextPage = () => (
+  <div className="text-blue-600">website</div>
+);
 
 export default Page;
